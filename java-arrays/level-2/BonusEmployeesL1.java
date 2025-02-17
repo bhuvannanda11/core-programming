784faed46ac5b4b590f68e25df81e6de5f277a44
bhuvannanda0011 <bhuvannanda0011@gmail.com>
2025-02-17 10:12:03 +0530

    First commit

---
+++ java-arrays/level-2/BonusEmployeesL1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class BonusEmployeesL1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double salary[] = new double[10];
+        int yearsOfService[] = new int[10];
+        double bonusAmount[] = new double[10];
+        double newSalary[] = new double[10];
+
+        double totalBonus = 0;
+        double totalOldSalary = 0;
+        double totalNewSalary = 0;
+
+        for(int i=0;i<10;i++){
+            while(true){
+                
+            }
+        }
+
+        sc.close();
+    }
+}
